cda48196ae40837969386a5aba5de19095dc9bd7
1806PiYuSh1806 <piyushupadhyay37756@gmail.com>
2025-01-14 23:24:28 +0530

    updated links

--- client/src/components/FrontPage/FrontPage.js
+++ client/src/components/FrontPage/FrontPage.js
@@ -1,36 +1,68 @@
-import { Button, Container, Grid, Typography } from "@material-ui/core";
+import { Typography, AppBar, Toolbar, Avatar, Button } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import useStyles from "./styles";
-import HomeImg from "../../images/home_page.png";
-import { Link } from "react-router-dom";
+import { LOGOUT } from "../../constants/actionTypes";
 
-const FrontPage = () => {
+const Navbar = () => {
   const classes = useStyles();
-  const user = JSON.parse(localStorage.getItem("profile"));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    const token = user?.token;
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.exp * 1000 < new Date().getTime()) {
+        // if expiry time in millisecs is less than current time in millisecs
+        logout();
+      }
+    }
+    setUser(JSON.parse(localStorage.getItem("profile")));
+    // eslint-disable-next-line
+  }, [location]);
+
+  const logout = () => {
+    dispatch({ type: LOGOUT });
+    history.push("/");
+    setUser(null);
+  };
+
   return (
-    <Container>
-      <Grid container className={classes.gridContainer} spacing={3}>
-        <Grid className={classes.gridItem} item sm={12} md={6}>
-          <Typography className={classes.headline}>
-            Think Less
-            <br /> Sleep Better
-            <br /> Be Happier
-            <br />
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.mainContainer}>
+        {user ? (
+          <Typography component={Link} to="/home" className={classes.heading} variant="h4" align="center">
+            aeiou
           </Typography>
-          {user ? (
-            <Button component={Link} to="/quiz" className={classes.button} size="large" variant="contained">
-              Start Now
+        ) : (
+          <Typography component={Link} to="/" className={classes.heading} variant="h4" align="center">
+            aeiou
+          </Typography>
+        )}
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>
+              {user?.result.name.charAt(0)}
+            </Avatar>
+            <Button variant="contained" className={classes.loginButton} onClick={logout}>
+              Log Out
             </Button>
-          ) : (
-            <Button component={Link} to="/auth" className={classes.button} size="large" variant="contained">
-              Start Now
-            </Button>
-          )}
-        </Grid>
-        <Grid className={classes.gridItem} item sm={12} md={5}>
-          <img src={HomeImg} alt="A girl on phone" />
-        </Grid>
-      </Grid>
-    </Container>
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" className={classes.loginButton} variant="contained">
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
-export default FrontPage;
+
+export default Navbar;
