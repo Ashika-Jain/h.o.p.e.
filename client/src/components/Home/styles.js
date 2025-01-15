3bdbb740ca8eb268bbb54d1f58854665ba7a2277
Ashika Jain <ashikajainkol@gmail.com>
2025-01-15 20:34:40 +0530

    changed home page ui

--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -1,4 +1,3 @@
-
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
@@ -18,25 +17,20 @@ export default makeStyles((theme) => ({
   gridContainer: {
     padding: "10px",
   },
-  heroContainer:{
-    display:"flex",
-    width:"80%",
-    margin:"0 auto",
-    justifyContent:"space-evenly"
+  box: {
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    borderRadius: "5px",
+    border: "1px solid #068fa0",
   },
-  heroItemContainer:{
-    boxShadow:"0 1rem 2rem rgba(0,0,0,.2)",
-    padding:".5rem 1.75rem",
-    textAlign:"center",
-    borderRadius:"20px"
+  button: {
+    color: "#fff",
+    marginTop: '0.6rem',
+    marginBottom: '0.5rem',
+    backgroundColor: "#068fa0",
+    borderRadius: 15,
+    "&:hover": {
+      backgroundColor: "#468fa0",
+    },
   },
-  heroItemHead:{
-    color:"#222222",
-  },
-  heroItemSpan:{
-    color:"#3f51b5"
-  },
-  heroItemScore:{
-    color:"#3f51b5"
-  }
 }));
