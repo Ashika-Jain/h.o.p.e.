ed48cb77b53bf7f266d002e2df72a17bf8b1e6e5
Ashika Jain <ashikajainkol@gmail.com>
2025-01-15 20:09:43 +0530

    implemented counter

--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -17,18 +17,25 @@ export default makeStyles((theme) => ({
   gridContainer: {
     padding: "10px",
   },
-  box: {
-    alignItems: "center",
-    backgroundColor: "#ffffff",
-    borderRadius: "5px",
-    border: "1px solid #068fa0",
+  heroContainer:{
+    display:"flex",
+    width:"80%",
+    margin:"0 auto",
+    justifyContent:"space-evenly"
   },
-  button: {
-    color: "#fff",
-    backgroundColor: "#068fa0",
-    borderRadius: 15,
-    "&:hover": {
-      backgroundColor: "#468fa0",
-    },
+  heroItemContainer:{
+    boxShadow:"0 1rem 2rem rgba(0,0,0,.2)",
+    padding:".5rem 1.75rem",
+    textAlign:"center",
+    borderRadius:"20px"
   },
+  heroItemHead:{
+    color:"#222222",
+  },
+  heroItemSpan:{
+    color:"#3f51b5"
+  },
+  heroItemScore:{
+    color:"#3f51b5"
+  }
 }));
