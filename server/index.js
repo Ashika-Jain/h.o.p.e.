d7d6ff1eb6026de459a69b1795a142fcd2ef3753
Ashika Jain <ashikajainkol@gmail.com>
2025-01-17 13:03:16 +0530

    db_uri fix

--- server/index.js
+++ server/index.js
@@ -88,15 +88,19 @@ app.get("/", (req, res) => {
 // MongoDB Connection and Server
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
-  )
-  .catch((err) => console.log(err));
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to database"); // Message after successful connection
+
+    app.listen(PORT, () => {
+      console.log(`Server running on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+};
+
+startServer();
 
 mongoose.set("useFindAndModify", false);
